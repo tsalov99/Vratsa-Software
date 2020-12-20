fa35543689b39bc0addfb5b22f5a5e18756516e6
Tihomir Salov <ttihomirr@gmail.com>
2020-12-20 17:18:19 +0200

    Create cars-array.php

---
+++ arrays/cars-array.php
@@ -0,0 +1,8 @@
+<?php 
+
+$cars = [["brand" => "Toyota", "Supra" => "v50", "year" => "1994", "price" => 120000, "sales" => 148], 
+["brand" => "Ford", "model" => "Mustang", "year" => "2014", "price" => 65000, "sales" => 340],
+["brand" => "Porsche", "" => "Panamera", "year" => "2012", "price" => 73000, "sales" => 568],];
+
+echo $cars[1]["year"];
+?>
