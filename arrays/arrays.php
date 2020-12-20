39a0ecc92dabef986f81c427e2adb794bbf9e336
Tihomir Salov <ttihomirr@gmail.com>
2020-12-20 16:14:54 +0200

    asd
    asd

---
+++ arrays/arrays.php
@@ -0,0 +1,5 @@
+<?php 
+$countries = ["BG" => ["name" => "Bulgaria", "capital" => "Sofia", "population" => 7000000,], 
+"IT" => ["name" => "Bulgaria", "capital" => "Sofia", "biggestCities" => ["milano", "venezia"]], 
+"IT" => ["name" => "Bulgaria", "capital" => "Sofia"],]
+?>
---
+++ arrays/intro.php
@@ -0,0 +1,16 @@
+<?php 
+$car = "Volvo";
+$car = "BMW";
+$car = "VW";
+
+$arr = ["Volvo", "BMW", "Vw",];
+
+$car1 = ["brand" => "Volvo", "model" => "v50", "year" => "2004", "price" => 5000,];
+$car2 = ["brand" => "BMW", "model" => "m5", "year" => "2010", "price" => 50000,];
+$car2 = ["brand" => "Vw", "model" => "golf", "year" => "2010", "price" => 13000,];
+
+$cars = [["brand" => "Volvo", "model" => "v50", "year" => "2004", "price" => 5000,], 
+$car2 = ["brand" => "Vw", "model" => "golf", "year" => "2010", "price" => 13000,];
+["brand" => "BMW", "model" => "m5", "year" => "2010", "price" => 50000,],];
+
+?>
