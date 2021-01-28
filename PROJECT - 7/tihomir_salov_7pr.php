ed44cb5505da3c8793b67277dbf2920edfa248ea
tsalov99 <74863393+tsalov99@users.noreply.github.com>
2021-01-28 17:51:00 +0200

    Update tihomir_salov_7pr.php

--- PROJECT - 7/tihomir_salov_7pr.php
+++ PROJECT - 7/tihomir_salov_7pr.php
@@ -1,2 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<form action="" method="POST">
+<input type="text" name="info">
+<input type="submit" name="submit">
+</form>
+</body>
 
+<?php
+$info = $_POST;
