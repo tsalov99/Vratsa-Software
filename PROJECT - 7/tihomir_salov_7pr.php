d1cce0fde5ebbf4c8c9e9852d85147103dc799ae
tsalov99 <74863393+tsalov99@users.noreply.github.com>
2021-01-29 15:57:40 +0200

    awdawdqwiugdqwt656
    adsadkajsdhgaisudol;

--- PROJECT - 7/tihomir_salov_7pr.php
+++ PROJECT - 7/tihomir_salov_7pr.php
@@ -7,9 +7,20 @@
 <body>
 <form action="" method="POST">
 <input type="text" name="info">
-<input type="submit" name="submit">
+<input type="submit">
 </form>
 </body>
 
 <?php
-$info = $_POST;
+
+$info = implode($_POST);
+var_dump($info);
+echo "<p></p>";
+$info = str_replace(" ", "", $info);
+$info = str_replace("]", "", $info);
+$info = str_replace("[", "", $info);
+$info = explode(",", $info);
+
+foreach($info as $call) {
+    print_r ($call . "<br>"); 
+}
