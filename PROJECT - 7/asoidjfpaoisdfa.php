db486a695131bc037512dda7937851770c0c55e5
tsalov99 <74863393+tsalov99@users.noreply.github.com>
2021-02-19 14:49:40 +0200

    Update asoidjfpaoisdfa.php

--- PROJECT - 7/asoidjfpaoisdfa.php
+++ PROJECT - 7/asoidjfpaoisdfa.php
@@ -12,6 +12,9 @@
 </body>
 
 <?php
+$projects = [];
+$subprojects = [];
+$methods = [];
 $result = [];
 $info = implode($_POST);
 //var_dump($result);
@@ -25,36 +28,6 @@ $info = explode(",", $info);
 foreach($info as $calls) {
     $calls = explode("/", $calls);
     $calls = array_slice($calls, 1, count($calls) - 1);
-
-    if(empty($result)) {
-        $result = ["$calls[0]" => 1,["$calls[1]" => 1,["$calls[2]" => 1]]];
-        var_dump($result);
-        echo "<br>";
-
-        } else {
-            if (!array_key_exists($calls[0], $result)) {
-                array_push($result, ["$calls[0]"]);
-                $result["$calls[0]"] = 1;
-
-                if (!array_key_exists($calls[1], $result)) {
-                    $result["$calls[1]"] = 1;
-
-                    if(!array_key_exists($calls[2], $result)) {
-                        $result["$calls[2]"] = 1;
-
-                    } else {
-                        $result["$calls[2]"] += 1;
-                    }
-                } else {
-                    $result["$calls[1]"] += 1;
-                }
-                //print_r($result["projects"]);
-                //echo "<br>";
-            } else {
-                $result["$calls[0]"] += 1;
-            }
-        }
-    //print_r($result);
-    //echo "<br>";
+    var_dump($calls);
 }
-var_dump($result);
+//var_dump($result);
