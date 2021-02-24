bb33b77cce6bdc09251cb3efe9050a2dc1531927
tsalov99 <74863393+tsalov99@users.noreply.github.com>
2021-02-24 11:43:02 +0200

    Update asoidjfpaoisdfa.php

--- PROJECT - 7/asoidjfpaoisdfa.php
+++ PROJECT - 7/asoidjfpaoisdfa.php
@@ -12,7 +12,7 @@
 </body>
 
 <?php
-$projects = [];
+$projects = [["project1" => 1, ]];
 $subprojects = [];
 $methods = [];
 $result = [];
@@ -24,10 +24,25 @@ $info = str_replace("]", "", $info);
 $info = str_replace("[", "", $info);
 $info = str_replace('"', "", $info);
 $info = explode(",", $info);
-//print_r($info);
+
 foreach($info as $calls) {
     $calls = explode("/", $calls);
     $calls = array_slice($calls, 1, count($calls) - 1);
-    var_dump($calls);
+
+    if(empty($result)) {
+        $result = ["$calls[0]" => [$calls[1], $calls[2]]];
+        //var_dump($result);
+    } else {
+        if (array_key_exists($calls[0], $result)) {
+            array_push($result["$calls[0]"], $calls[1], $calls[2]);
+            //var_dump($result);
+            //echo "<br>";
+        } else {
+            array_push($result, ["$calls[0]" => [$calls[1], $calls[2]]]);
+            var_dump($result);
+            echo "<br>";
+        }
+    }
+    //var_dump($calls);
 }
 //var_dump($result);
