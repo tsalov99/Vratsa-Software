dcd4041c64ed4f9ea809b4db17fcb44cb6ae4d50
tsalov99 <74863393+tsalov99@users.noreply.github.com>
2021-02-26 12:24:35 +0200

    oop
    oop

---
+++ OOP/training.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Footballers {
+    public $name;
+    public $number;
+    function __construct($a, $b) {
+        $this-> name = $a;
+        $this-> number = $b;
+    }
+}
+
+$players = [["Ivo Ivanov", 8], ["Bai Tishe", 3], ["Kantar4o", 5]];
+
+foreach ($players as $player) {
+    $a = $player[0];
+    $b = $player[1];
+    $igra4 = new Footballers($a, $b);
+    //print_r($igra4);
+}
+print_r($igra4);
+?>
